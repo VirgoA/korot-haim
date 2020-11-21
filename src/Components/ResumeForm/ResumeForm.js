1a52cfdfe1ad981648d81e9df391de156111ee27
oleg <vinodevmail@gmail.com>
2020-11-21 12:51:58 +0200

    ResumeForm as functional component with useState hook

--- src/Components/ResumeForm/ResumeForm.js
+++ src/Components/ResumeForm/ResumeForm.js
@@ -1,56 +1,31 @@
 import BasicInput from "../BasicInput";
 import "./resumeForm.css";
 
-import React, { Component } from "react";
+import React, {useState} from "react";
 
-export default class ResumeForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "",
-      lastName: "",
-      email: "",
-      phone: "",
-    };
-    this.setName = this.setName.bind(this);
-    this.setLastName = this.setLastName.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setPhoneNumber = this.setPhoneNumber.bind(this);
-  }
+const ResumeForm = () => {
 
-  setName(newName) {
-    console.log("changing name state to:" + newName);
-    this.setState({ name: newName });
-  }
-  setLastName(newLastName) {
-    console.log("changing lastname state to:" + newLastName);
-    this.setState({ lastName: newLastName });
-  }
-  setEmail(newEmail) {
-    console.log("changing email state to:" + newEmail);
-    this.setState({ email: newEmail });
-  }
-  setPhoneNumber(newNumber) {
-    console.log("changing phone # state to:" + newNumber);
-    this.setState({ phone: newNumber });
-  }
+  const [name, setName] = useState()
+  const [laseName, setLastName] = useState()
+  const [email, setEmail] = useState()
+  const [phoneNumber, setPhoneNumber] = useState()
 
-  render() {
-    return (
-      <form>
-        <h3>פרטים אישיים</h3>
-        <div className="form-section">
-          <div className="row-inputs">
-            <BasicInput name={"שם משפחה"} handleState={this.setLastName} />
-            <BasicInput name={"שם פרטי"} handleState={this.setName} />
-          </div>
-
-          <div className="row-inputs">
-            <BasicInput name={"מייל"} handleState={this.setEmail} />
-            <BasicInput name={"טלפון"} handleState={this.setPhoneNumber} />
-          </div>
+  return (
+    <form>
+      <h3>פרטים אישיים</h3>
+      <div className="form-section">
+        <div className="row-inputs">
+          <BasicInput name={"שם משפחה"} handleState={setLastName} />
+          <BasicInput name={"שם פרטי"} handleState={setName} />
         </div>
-      </form>
-    );
-  }
+
+        <div className="row-inputs">
+          <BasicInput name={"מייל"} handleState={setEmail} />
+          <BasicInput name={"טלפון"} handleState={setPhoneNumber} />
+        </div>
+      </div>
+    </form>
+  );
 }
+
+export default ResumeForm
