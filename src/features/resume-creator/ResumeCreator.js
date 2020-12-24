1151df5985561e217e994c734eaabb8a81c9eb85
oleg <vinodevmail@gmail.com>
2020-12-24 16:19:58 +0200

    included DisplayResume component

--- src/features/resume-creator/ResumeCreator.js
+++ src/features/resume-creator/ResumeCreator.js
@@ -6,11 +6,10 @@ const ResumeCreator = () => {
     return (
         <div className="container">
             <div className="resume-control">
-                {/* <div>קורות חיים</div> */}
                 <ResumeForm/>
             </div>
             <div className="resume-preview">
-                {/* <h1>פה יוצג הקורות חיים</h1> */}
+                <DisplayResume />
             </div>
         </div>    
     )
