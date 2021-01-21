d8eb9e45cd526d30c32aab3997d4e0f4f725c3c1
Tal Gold <talpogorelis@gmail.com>
2021-01-21 04:41:59 +0200

    added download functionallity - api's response pdf

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,20 @@
+import download from "downloadjs";
+import axios from "axios";
+
 const url = "http://localhost:3001";
 
 export const downloadResume = async (html) => {
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-Type': "application/json"
-            },
-            body: JSON.stringify({html: html})
-        })
-        console.log(response);
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    let response = await axios.post(url, JSON.stringify({ html: html }), {
+      responseType: "arraybuffer",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/pdf",
+      },
+    });
+    let pdf = new Blob([response.data], { type: "application/pdf" });
+    download(pdf, "korothaim.pdf");
+  } catch (error) {
+    return error;
+  }
+};
