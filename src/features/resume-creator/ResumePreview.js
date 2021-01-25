feb6247c05132f57017315c1b76cf39dedccb039
Tal Gold <talpogorelis@gmail.com>
2021-01-25 05:55:53 +0200

    added delay on download requests

--- src/features/resume-creator/ResumePreview.js
+++ src/features/resume-creator/ResumePreview.js
@@ -5,16 +5,17 @@ import { Button } from "@material-ui/core";
 import GetAppRoundedIcon from "@material-ui/icons/GetAppRounded";
 import "./displayResume.css";
 import { downloadResume } from "../../api";
-import exampleData from "../../utils/example_data.json"
-import ReactDOMServer from 'react-dom/server'
+import exampleData from "../../utils/example_data.json";
+import ReactDOMServer from "react-dom/server";
 
 function ResumePreview(props) {
   const data = useSelector((state) => state.form);
+  const [downloadAvailable, setAvailability] = useState(true);
 
   return (
     <div className="resume-preview">
       <div id="resume-capture">
-        <BlueTemplate data={data}/>
+        <BlueTemplate data={data} />
       </div>
       <div className="resume-preview-controls">
         <Button
@@ -22,13 +23,7 @@ function ResumePreview(props) {
           size="large"
           color="secondary"
           id="download-btn"
-          onClick={() => {
-            downloadResume(
-              ReactDOMServer.renderToStaticMarkup(
-                <BlueTemplate data={data}/>
-              )
-            );
-          }}
+          onClick={downloadRequest}
         >
           הורד
           <GetAppRoundedIcon style={{ marginTop: "4px" }} />
@@ -36,6 +31,27 @@ function ResumePreview(props) {
       </div>
     </div>
   );
+
+  function downloadRequest() {
+    setAvailability(false);
+    submitRequest(downloadAvailable, data);
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        setAvailability(true);
+      }, 5000); //5 seconds delay.
+    });
+  }
+}
+
+function submitRequest(downloadAvailable, data) {
+  if (downloadAvailable) {
+    downloadResume(
+      ReactDOMServer.renderToStaticMarkup(<BlueTemplate data={data} />)
+    );
+  } else {
+    console.log("request canceled - download delay");
+    console.log("only 1 request is allowed every 5 seconds");
+  }
 }
 
 export default ResumePreview;
